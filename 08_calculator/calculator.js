8adc6edaedc899e2ea7a5ca111f0db0f704e1baf
Keiyan4 <184532673+Keiyan4@users.noreply.github.com>
2025-01-25 14:14:37 +0800

    build: make a calculator using JS

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,60 @@
-const add = function() {
+const add = function(first, second) {
+	
+  let sum = first + second;
+  return sum;
+};
+
+add(0,0);
+
+const subtract = function(first, second) {
+
+  let difference = first - second;
+  return difference;
+
+};
+subtract(10,6);
+
+
+
+const sum = function(arr) {
+ 
+  let reducedArr = arr.reduce((total, number) => total + number, 0);
+  return reducedArr;
+
 	
 };
+sum([]);
 
-const subtract = function() {
+const multiply = function(arr) {
+
+  let reducedProduct = arr.reduce((total, number) => total*number, 1);
+  return reducedProduct;
+
+};
+multiply([]);
+
+
+
+const power = function(first, second) {
+
+  let poweredNumber = first ** second;
+  return poweredNumber;
 	
 };
+power(4,3);
 
-const sum = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1){
+    return 1;
+  }
+  let result = 1;
+  for (i=2; i<=num; i++){
+    result*= i;
+  }
+	return result;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
+factorial(0);
 // Do not edit below this line
 module.exports = {
   add,
