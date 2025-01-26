f5ba30166155ec0bd74bdc7ee3f37cbd23c237c1
Keiyan4 <184532673+Keiyan4@users.noreply.github.com>
2025-01-26 15:17:20 +0800

    Build: Fibonacci Sequence that returns a specific number of the sequence. Accepts strings, does not accept negative values.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    num = Number(num);
+
+    if (num !== parseInt(num)) return fibonacci;
+
+    if(num < 0) return "OOPS";
+    if(num === 0) return 0;
+    if(num === 1 || num === 2) return 1;
+
+    return fibonacci(num-1) + fibonacci(num-2);
+  
 };
+
+fibonacci(6);
 
 // Do not edit below this line
 module.exports = fibonacci;
