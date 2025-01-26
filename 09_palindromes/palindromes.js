68f0853f5b4f14b5b65140f6718175eef64fbf5e
Keiyan4 <184532673+Keiyan4@users.noreply.github.com>
2025-01-26 11:06:06 +0800

    build: Make palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    const alphanumeric = "abcdefghijklmnopqrstuvxyz0123456789";
+
+
+    let palindromesString = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanumeric.includes(char))
+    .join("");
+
+    let reversedString = palindromesString.split("").reverse().join("");
+
+    return reversedString === palindromesString;
+
+    
+
 
 };
+
+palindromes("");
 
 // Do not edit below this line
 module.exports = palindromes;
