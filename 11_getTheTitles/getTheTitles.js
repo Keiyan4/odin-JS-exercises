76202e8984f88ece602e75f3087860c58e0fbe58
Keiyan4 <184532673+Keiyan4@users.noreply.github.com>
2025-01-26 22:56:19 +0800

    Build: creates an array using object then returned the title in the array.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
-};
+    const mappedBooks = books.map(book => book.title);
+    return mappedBooks;
+
+    };
+
+    const books = [
+        {
+            title: "Book1",
+            author: "Secret1"
+        },
+        {
+            title: "Book2",
+            author: "Secret2"
+        }
+    ];
+
+    getTheTitles(books);
+    
 
 // Do not edit below this line
 module.exports = getTheTitles;
